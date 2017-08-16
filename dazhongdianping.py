e6361cd20157cd0cd5c77c95da268e63649e7a3e
jochen <942814208@qq.com>
2017-08-16 09:25:30 +0800

    大众点评日料

--- dazhongdianping.py
+++ dazhongdianping.py
@@ -1,12 +1,11 @@
 #!/usr/bin/Python
 # -*- coding: utf-8 -*-
-
-
-
-#获取大众点评上的南京地区所有日本料理店的名字，地址和人均消费
+#获取大众点评上的南京地区所有日本料理店的名字，地址和人均消费     #大众点评反爬虫比较严格，大概录入不到三百的数据就会出现录入错误。。有待改进爬虫
 import requests
 from bs4 import BeautifulSoup
 import pymysql
+import time
+import random
 
 conn = pymysql.connect(host = '127.0.0.1',port = 3306,user = 'root',password = '',db = 'y1',charset = 'utf8mb4',cursorclass = pymysql.cursors.DictCursor)
 cur = conn.cursor()
@@ -14,8 +13,8 @@ cur = conn.cursor()
 base_url = 'http://www.dianping.com/search/category/5/10/g113p'
 Headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'}
 
+for f in range(18,44):        #一共43页 range(1,44)
 
-for f in range(1,44):        #一共43页 range(1,44)
     url = base_url+'%d'%(f)
     html = requests.get(url,headers = Headers).content
     # print(type(html))
@@ -29,16 +28,28 @@ for f in range(1,44):        #一共43页 range(1,44)
     c = []
 
     for t in allname:
-        title = t.find('h4').text                       #title的类型此时为字符串        字符串和列表都可以直接存入数据库
-        a.append(title)
+        try:
+            title = t.find('h4').text                       #title的类型此时为字符串        字符串和列表都可以直接存入数据库
+            a.append(title)
+        except:
+            a.append('xxx')
+            print('一条数据录入allname出错')
 
     for y in alladdr:
-        addr = y.find('span',{'class':'addr'}).text
-        b.append(addr)
+        try:
+            addr = y.find('span',{'class':'addr'}).text
+            b.append(addr)
+        except:
+            b.append('xxx')
+            print('一条数据录入alladdr出错')
 
     for u in allprice:
-        price = u.find('b').text
-        c.append(price)
+        try:
+            price = u.find('b').text
+            c.append(price)
+        except:
+            c.append('xxx')
+            print("一条数据录入allprice出错")
     x = 0
     while x<len(a) :
         cur.execute("insert into y1(name,place,price) values ('%s','%s','%s')"%(a[x],b[x],c[x]))
