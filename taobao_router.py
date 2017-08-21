c4f2f814cb78f60508246c031265694d755c48f1
jochen <942814208@qq.com>
2017-08-21 23:28:23 +0800

    淘宝路由器

--- taobao_router.py
+++ taobao_router.py
@@ -1,7 +1,7 @@
 #!/usr/bin/Python
 # -*- coding: utf-8 -*-
 import requests
-from bs4 import BeautifulSoup
+import re
 
 baseurl = 'https://s.taobao.com/search?q=%E8%B7%AF%E7%94%B1%E5%99%A8&s='    # s=1,48,96 第一页  第二页 第三页
 Headers = {'user-agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'}
@@ -12,6 +12,5 @@ for k in range(0,79):
     response = requests.get(url,headers = Headers).content
     # print(response)
 
-    soup = BeautifulSoup(response,'html.parser')
-    title = soup.select('.product-title')
+    title = re('title(.*?)pic_url',response)
     print(len(title))
