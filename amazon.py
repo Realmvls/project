1feca8bbe2ff8ffa482286e930421f2834913bcc
jochen <942814208@qq.com>
2017-09-02 21:28:08 +0800

    amazon phone

--- amazon.py
+++ amazon.py
@@ -5,17 +5,19 @@
 #所有图片以手机名称命名+价格命名并下载到本地
 #多线程 共216964条数据
 
+#sql语句还没实现
+
 import requests
 from bs4 import BeautifulSoup
 import re
+import pymysql
 Headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'}
-url = 'https://www.amazon.cn/s/ref=nb_sb_noss_1?__mk_zh_CN=%E4%BA%9A%E9%A9%AC%E9%80%8A%E7%BD%91%E7%AB%99&url=search-alias%3Daps&field-keywords=%E6%89%8B%E6%9C%BA&rh=i%3Aaps%2Ck%3A%E6%89%8B%E6%9C%BA'
 allname = []
 alltitle = []
 allprice = []
 allurl = []
-allpicture = []
 img = []
+urllist = []
 def gethtml(url):
     try:
         html = requests.get(url,headers = Headers).content
@@ -75,24 +77,39 @@ def getpicture(soup):
         pictures = soup.select('.s-access-image.cfMarker')
         imgs = re.findall('src="(.*?)"',str(pictures))
         print('获取图片个数{}个'.format(len(imgs)))
-        for picture in imgs:
-            # print(picture)
-            allpicture.append(picture)
-        for i, j in enumerate(allpicture):
-            num = allname[i]
-            with open('F:/spider_data/amazon图片/{}.jpg'.format(i), 'wb') as file:
+        for i, j in enumerate(imgs):
+            with open('F:/spider_data/amazon图片/第{}页，第{}张.jpg'.format(page,i), 'wb') as file:         #用format填充两个参数时的用法
                 file.write(requests.get(j).content)
                 print('正在保存第{}张图片'.format(i))
         return
     except Exception as e:
         print('下载图片失败')
         print(e)
-url = 'https://www.amazon.cn/s/ref=nb_sb_noss_1?__mk_zh_CN=%E4%BA%9A%E9%A9%AC%E9%80%8A%E7%BD%91%E7%AB%99&url=search-alias%3Daps&field-keywords=%E6%89%8B%E6%9C%BA&rh=i%3Aaps%2Ck%3A%E6%89%8B%E6%9C%BA'
-soup = gethtml(url)
-getname(soup)
-gettitle(soup)
-getprice(soup)
-geturl(soup)
-getpicture(soup)
+def url_list(base_url):
+    for i in range(1,401):
+        url = base_url.format(i,i)
+        urllist.append(url)
+    return urllist
 
+conn = pymysql.connect(host = '127.0.0.1',port = 3306,user = 'root',password = '',db = 'amazon',charset = 'utf8mb4',cursorclass = pymysql.cursors.DictCursor)
+cur = conn.cursor()
+base_url = 'https://www.amazon.cn/s/ref=sr_pg_{}?fst=as:on&rh=k:手机,n:664978051&page={}&keywords=手机&ie=UTF8&qid=1504354063'
+url_list(base_url)
+page = 1   #用来记录抓取的页数
+num = 0    #用来记录第几条数据
+for url in urllist:
+    print('第{}页'.format(page))
+    soup = gethtml(url)
+    getname(soup)
+    gettitle(soup)
+    getprice(soup)
+    geturl(soup)
+    for num in range(len(allname)+1):
+        cur.execute('insert into phone (name,title,price,url) values (allname[num],alltitle[num],allprice[num],allurl[num]);')
+        num = num + 1
+        print('正在写入第{}条数据'.format(num))
+    getpicture(soup)
+    page = page+1
+cur.close()
+conn.close()
 
